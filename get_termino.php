1d85422515c590876c56ec6a2ffcdae5c8bda6b0
Brayan Rosales <brayan309giron@gmail.com>
2025-12-02 03:03:38 +0000

    Edit get_termino.php

--- get_termino.php
+++ get_termino.php
@@ -1,14 +1,28 @@
 <?php
 include "conexion.php";
 
-$id = $_GET['id'];
+if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+    echo json_encode(['error' => 'ID inválido']);
+    exit();
+}
 
-$sql = "SELECT t.palabra, t.definicion, u.nombre AS estudiante
-        FROM termino t
-        INNER JOIN usuario u ON u.id_Usuario = t.id_Usuario
-        WHERE t.id_Termino = $id";
+$id = intval($_GET['id']);
 
-$res = $conn->query($sql);
-$data = $res->fetch_assoc();
+$stmt = $conn->prepare("SELECT t.palabra, t.definicion, u.nombre AS estudiante
+                       FROM termino t
+                       INNER JOIN usuario u ON u.id_Usuario = t.id_Usuario
+                       WHERE t.id_Termino = ?");
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$res = $stmt->get_result();
 
-echo json_encode($data);
+if ($res->num_rows > 0) {
+    $data = $res->fetch_assoc();
+    echo json_encode($data);
+} else {
+    echo json_encode(['error' => 'Término no encontrado']);
+}
+
+$stmt->close();
+$conn->close();
+?>
